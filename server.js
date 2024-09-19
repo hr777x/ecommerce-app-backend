4efca9f55bd5badffd4e15d32c656dc6ad69a5ae
Haneef Ur Rehman <122209293+hr777x@users.noreply.github.com>
2024-09-19 02:52:42 -0700

    Added cookie-parser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyparser from 'body-parser'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import { connectDB } from './config/db.js'
 import userRoute from './routes/user-routes.js'
 import productRoute from './routes/product-routes.js'
@@ -13,6 +14,8 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyparser.urlencoded({extended: true}))
 app.use(bodyparser.json())
+app.use(cookieParser())
+
 app.use('/',userRoute)
 app.use('/api/products', productRoute);
 const port = process.env.port || 8080
